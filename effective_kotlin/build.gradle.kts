fad682611d68884c0ff1e33086beff1f064edb55
kadensungbincho <kadensungbincho@gmail.com>
2021-12-31 09:20:07 +0900

    delete build and add .gitignore, ch1

--- effective_kotlin/build.gradle.kts
+++ effective_kotlin/build.gradle.kts
@@ -14,6 +14,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework:spring-core:5.3.14")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.0.0")
 
     testImplementation(kotlin("test"))
 }
